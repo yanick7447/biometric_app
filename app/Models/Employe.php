63a949e20484a3886b1e49c08bfd06ac09a02e5d
STEVETENAS <stevetenadjang@gmail.com>
2022-08-15 16:06:34 +0100

    - addition of soft delete option on models

--- app/Models/Employe.php
+++ app/Models/Employe.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Employe extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'user_id','poste_id','role_id','admin_id',
--- app/Models/Equipe.php
+++ app/Models/Equipe.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Equipe extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
      protected $fillable = [
          'etat_id', 'user_id', 'objectif', 'lieu_travail',
          'long', 'lat', 'debut', 'fin',
