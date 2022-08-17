ca724f6edbe19b2c2ce7b156de6e59d63c056129
STEVETENAS <stevetenadjang@gmail.com>
2022-08-17 06:52:13 +0100

    - creation of routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\Http\Controllers\API\EmployeController;
+use App\Http\Controllers\API\EquipeController;
+use App\Http\Controllers\API\EquipeEmployeController;
+use App\Http\Controllers\API\EtatController;
+use App\Http\Controllers\API\JourTravailController;
+use App\Http\Controllers\API\PointageController;
+use App\Http\Controllers\API\PosteController;
+use App\Http\Controllers\API\RoleController;
+use App\Http\Controllers\API\TypePointageController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +27,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('user', UserController::class);
+Route::apiResource('employe', EmployeController::class);
+Route::apiResource('equipe', EquipeController::class);
+Route::apiResource('equipe-employe', EquipeEmployeController::class);
+Route::apiResource('etat', EtatController::class);
+Route::apiResource('jour-travail', JourTravailController::class);
+Route::apiResource('pointage', PointageController::class);
+Route::apiResource('type-pointage', TypePointageController::class);
+Route::apiResource('poste', PosteController::class);
+Route::apiResource('role', RoleController::class);
